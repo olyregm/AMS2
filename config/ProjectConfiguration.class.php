dd6dee81f3a84c66db5a6a544f3de273805504f1
Humbs <root@regatta-webcam-server.(none)>
2011-11-25 12:48:24 +0100

    plugins einzeln gelistet

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,5 +1,7 @@
 <?php
 
+// die naechste Zeile kann je nach installation anders sein
+// '/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php' - bei installation nach jobeet
 require_once '/usr/share/php5/PEAR/symfony/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
